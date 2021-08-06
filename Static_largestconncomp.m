03d2b75d09e15b9b34ad67eed4c34f24b4fd3375
Saidas2021 <87606094+Saidas2021@users.noreply.github.com>
2021-08-06 21:05:50 +0530

    Update Static_largestconncomp.m

--- Static_largestconncomp.m
+++ Static_largestconncomp.m
@@ -1,6 +1,36 @@
-% To successively find the largest connected component in a graph 'c' post removal of a 
-%set of nodes specified in vector 'd'.
-function [a,b]=Static_largestconncomp(c,d)
+function [a,b]=static_largestconncomp(c,d)
+% The function successively computes the largest connected component of the
+% network post diruption of nodes specified in a node list. It does
+% not remove nodes based on any dynamic recalculation of centrality
+% measures and does so statically based on a node list.
+%
+%  USAGE
+%
+% [a,b]=static_largestconncomp(c,d);
+%
+% INPUT ARGUMENTS
+%   c - The network from which the given nodes have to be disrupted in that
+%   order
+%   d - The node list containing the node numbers to be removed in a
+%   successive fashion
+%
+% OUTPUT ARGUMENTS
+%   a - the graph from which the nodes specified in the node list have been
+%   disrupted
+%   b - an array containing the largest connected component in the network
+%   post successive node deletions
+%
+% EXAMPLES
+%
+% The codeline mentioned below will disrupt the nodes mentioned in the node
+% list 'd' from the network 'c'
+% 
+% [a,b]=static_largestconncomp(c,d);
+%
+% AUTHORS
+% Sai Saranga Das M (Department of Biotechnology, IIT Madras)
+% Dr. Karthik Raman (Department of Biotechnology, IIT Madras)
+
 e=size(d);
 b=[];
 [g,h]=conncomp(c);
@@ -11,3 +41,4 @@ c=rmnode(c,d(1,i));
 b(1,i+1)=max(h);
 end
 a=c;
+
