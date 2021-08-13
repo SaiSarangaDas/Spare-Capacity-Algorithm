d93d17d7db536ee2ffaecd25b84c35176fc765a8
Saidas2021 <87606094+Saidas2021@users.noreply.github.com>
2021-08-13 23:24:04 +0530

    Add files via upload

---
+++ seedgraph.m
@@ -0,0 +1,37 @@
+function b=seedgraph(numnode,numedge)
+% This function returns an seed graph having a given number of nodes
+% and edges. 
+%
+% USAGE
+% 
+% b=seedgraph(numnode,numedges);
+%
+% INPUT ARGUMENTS
+%   numnode - number of nodes in the network
+%   numedges - number of edges in the network
+%  
+% OUTPUT ARGUMENTS
+%   b - the desired Erdos_Reyni network  
+%
+% EXAMPLES
+%
+% The codeline mentioned below will return an Erdos_Reyni network of 10
+% nodes and 10 edges 
+%  
+% b=seedgraph(10,10);
+%
+% AUTHORS
+% Sai Saranga Das M (Department of Biotechnology, IIT Madras)
+% Dr. Karthik Raman (Department of Biotechnology, IIT Madras)
+
+a=ones(numnode);
+b=graph(a, 'omitselfloops');
+while numedges(b)>numedge
+c=round(1+(numnode-1)*rand());
+d=round(1+(numnode-1)*rand());
+while c==d
+d=round(1+(numnode-1)*rand());
+end
+b=rmedge(b,c,d);
+end
+end
